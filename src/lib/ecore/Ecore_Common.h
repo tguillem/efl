a07e9a6a3663a43981cecb7e138bb4f6185d361c
Daniel Kolesa <d.kolesa@samsung.com>
2014-08-01 15:11:28 +0100

    eolian: generate implicit typedef for each class
    And use these typedefs instead of just Eo in API.

--- src/lib/ecore/Ecore_Common.h
+++ src/lib/ecore/Ecore_Common.h
@@ -2524,6 +2524,8 @@ typedef enum _Ecore_Poller_Type Ecore_Poller_Type;
 
 typedef Eo    Ecore_Poller; /**< A handle for pollers */
 
+#define _ECORE_POLLER_EO_CLASS_TYPE
+
 /**
  * @brief Sets the time(in seconds) between ticks for the given poller type.
  * @param type The poller type to adjust.
@@ -2625,6 +2627,8 @@ typedef Eina_Bool (*Ecore_Timeline_Cb)(void *data, double pos);
  * @since 1.8
  */
 typedef Eo Ecore_Animator; /**< A handle for animators */
+
+#define _ECORE_ANIMATOR_EO_CLASS_TYPE
 
 /**
  * @brief Set the animator call interval in seconds.
@@ -2900,6 +2904,8 @@ EAPI void ecore_animator_custom_tick(void);
  */
 typedef Eo Ecore_Timer; /**< A handle for timers */
 
+#define _ECORE_TIMER_EO_CLASS_TYPE
+
 /**
  * Retrieves the current precision used by timer infrastructure.
  * @return Current precision.
@@ -3008,15 +3014,21 @@ EAPI char *ecore_timer_dump(void);
  */
 typedef Eo Ecore_Idler; /**< A handle for idlers */
 
+#define _ECORE_IDLER_EO_CLASS_TYPE
+
 /*
  * @since 1.8
  */
 typedef Eo Ecore_Idle_Enterer; /**< A handle for idle enterers */
 
+#define _ECORE_IDLE_ENTERER_EO_CLASS_TYPE
+
 /*
  * @since 1.8
  */
 typedef Eo Ecore_Idle_Exiter; /**< A handle for idle exiters */
+
+#define _ECORE_IDLE_EXITER_EO_CLASS_TYPE
 
 /**
  * @}
@@ -3050,6 +3062,8 @@ typedef Eo Ecore_Idle_Exiter; /**< A handle for idle exiters */
  * @since 1.8
  */
 typedef Eo Ecore_Job;    /**< A job handle */
+
+#define _ECORE_JOB_EO_CLASS_TYPE
 /**
  * @}
  */
