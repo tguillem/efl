74f562afcb8f264e6b9529b90372ac49ee04b2d0
Daniel Zaoui <daniel.zaoui@samsung.com>
2015-05-06 20:41:59 +0300

    Eolian/Generator: fix enums generation
    Typedef word was not generated for enums.
    
    @fix

--- src/bin/eolian/types_generator.c
+++ src/bin/eolian/types_generator.c
@@ -118,14 +118,11 @@ _type_generate(const Eolian_Type *tp, Eina_Bool in_typedef, Eina_Bool full)
                    break;
                 }
               char *pre = NULL;
-              eina_strbuf_append_printf(buf, "enum %s {\n", name);
+              eina_strbuf_append_printf(buf, "typedef enum\n{\n");
               if (eolian_type_enum_legacy_prefix_get(tp))
-                {
-                   pre = strdup(eolian_type_enum_legacy_prefix_get(tp));
-                   free(name);
-                }
+                pre = strdup(eolian_type_enum_legacy_prefix_get(tp));
               else
-                pre = name;
+                pre = strdup(name);
               eina_str_toupper(&pre);
               Eina_Iterator *members = eolian_type_enum_fields_get(tp);
               Eina_Bool next = eina_iterator_next(members, (void**)&member);
@@ -162,10 +159,11 @@ _type_generate(const Eolian_Type *tp, Eina_Bool in_typedef, Eina_Bool full)
                    if (desc) eina_strbuf_append_printf(buf, " /** %s */", desc);
                    eina_strbuf_append(buf, "\n");
                 }
+              eina_strbuf_append_printf(buf, "} %s", name);
               eina_strbuf_free(membuf);
+              free(name);
               free(pre);
               eina_iterator_free(members);
-              eina_strbuf_append(buf, "}");
               break;
            }
       default:
