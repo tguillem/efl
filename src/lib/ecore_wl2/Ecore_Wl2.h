725e56d46101b7519bbb841071137dc9ef2ccc0d
Chris Michael <cp.michael@samsung.com>
2015-08-18 10:08:38 -0400

    ecore-wl2: Add events for adding and removing globals
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/lib/ecore_wl2/Ecore_Wl2.h
+++ src/lib/ecore_wl2/Ecore_Wl2.h
@@ -29,6 +29,9 @@
 typedef struct _Ecore_Wl_Window Ecore_Wl_Window;
 # endif
 
+EAPI extern int ECORE_WL2_EVENT_GLOBAL_ADDED;
+EAPI extern int ECORE_WL2_EVENT_GLOBAL_REMOVED;
+
 EAPI int ecore_wl2_init(const char *name);
 
 /* # ifdef __cplusplus */
