80a09047e99022cd95ec16146ef7934ea9b9ea0e
Chris Michael <cp.michael@samsung.com>
2015-09-09 09:27:05 -0400

    ecore-wl2: Fix compile errors in ecore_wl2_window
    Signed-off-by: Chris Michael <cp.michael@samsung.com>

--- src/lib/ecore_wl2/ecore_wl2_window.c
+++ src/lib/ecore_wl2/ecore_wl2_window.c
@@ -34,7 +34,7 @@ EAPI int
 ecore_wl2_window_id_get(Ecore_Wl2_Window *window)
 {
    EINA_SAFETY_ON_NULL_RETURN_VAL(window, -1);
-   return win->id;
+   return window->id;
 }
 
 EAPI struct wl_surface *
@@ -42,9 +42,9 @@ ecore_wl2_window_surface_get(Ecore_Wl2_Window *window)
 {
    EINA_SAFETY_ON_NULL_RETURN_VAL(window, NULL);
 
-   if (!win->surface)
+   if (!window->surface)
      {
-        win->surface =
+        window->surface =
           wl_compositor_create_surface(window->display->wl.compositor);
      }
 
